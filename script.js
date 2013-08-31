39acd7ec5ab56a7752e0a6a1bc90477b1ee6602c
ka <ka.kaosf@gmail.com>
2013-08-31 23:35:33 +0900

    show map

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ $("button#imakoko").click(function () {
   navigator.geolocation.getCurrentPosition(
     function (p) {
       $("div#output").text("latitude: " + p.coords.latitude + ", longitude: " + p.coords.longitude);
+      var map = new ZDC.Map(document.getElementById('map'));
     },
     function (e) {
       $("div#output").text("ERROR");
