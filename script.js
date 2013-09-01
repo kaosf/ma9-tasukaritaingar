78b674f1fd0998472961c7fc87674a8415e86f9b
ka <ka.kaosf@gmail.com>
2013-09-01 13:42:06 +0900

    show a map defaultly

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
+var map;
+
+$(function () {
+  map = new ZDC.Map(document.getElementById('map'), {
+    zoom: 9
+  });
+});
+
 $("button#imakoko").click(function () {
   navigator.geolocation.getCurrentPosition(
     function (p) {
       var lat = p.coords.latitude;
       var lon = p.coords.longitude;
       $("div#output").text("latitude: " + lat + ", longitude: " + lon);
-      var map = new ZDC.Map(document.getElementById('map'), {
-        latlon: ZDC.wgsTotky(new ZDC.LatLon(lat, lon)),
-        zoom: 9
-      });
+      // TODO: set map's center with lat and lon
+      // ZDC.wgsTotky(new ZDC.LatLon(lat, lon))
     },
     function (e) {
       $("div#output").text("ERROR");
